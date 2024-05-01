0f9b67706cfdafd4e014b9d4ae49665e857480b6
Lightcode777 <102882499+nate177@users.noreply.github.com>
2024-05-01 14:39:29 -0300

    Update installhtop.sh

--- installhtop.sh
+++ installhtop.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-
+program2=ufw
 program=htop
 
 if command -v $program
 then
-	echo "$program is available, let's run it man..."
+	echo "$program is available, Now Running"
 else
 	echo "$program is NOT available, installing it....."
 	sudo pacman -Syu
 	sudo pacman -S htop
 fi
 
-$program
+if command -v $program2
+then
+	echo"$program2 is available,starting daemon with systemctl..."
+ else
+ 	echo "$program2 is NOT available, finding package1.......2.....3.......3.........5 also installing gufw GUI"
+  	sudo pacman -S ufw
+   	sudo pacman -S gufw
